a1cd2eb9e00746833340b32fd0dfa54895a7dd31
dostrel <jonathan.dostrel@epitech.eu>
2021-04-12 15:26:26 +0200

    teste

---
+++ src/app/App.java
@@ -0,0 +1,96 @@
+package app;
+import app.Controller.LoginController;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            this.primaryStage = primaryStage;
+            this.primaryStage.setTitle("Visit My Cities");
+
+            initRootLayout();
+
+            showLogin();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+
+    public void showLogin() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("view/Login.fxml"));
+            AnchorPane login = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(login);
+
+            LoginController controller = loader.getController();
+            controller.setMainApp(this);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public void showListeVilleview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("../views/ListeVille.fxml"));
+            AnchorPane listeVilleview = (AnchorPane) loader.load();
+            
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(listeVilleview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+    
+	/**
+	 * Returns the main stage.
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/app/Controller/LoginController.java
@@ -0,0 +1,13 @@
+package app.Controller;
+import app.App;
+import javafx.fxml.FXML;
+public class LoginController {
+
+    private App App;
+
+    public LoginController(){}
+    
+    public void setMainApp(App App) {
+        this.App = App;
+    }
+}
